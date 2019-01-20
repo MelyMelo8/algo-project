815935bb83c51fd997b6e331f0f50a5f660aaf2f
Z_runner <z.runner.mc@gmail.com>
2019-01-20 21:02:03 +0100

    Update proj1.pas

--- proj1.pas
+++ proj1.pas
@@ -3,6 +3,17 @@ unit proj1;
 {$mode objfpc}{$H+}
 {$codepage UTF8}
 {$I-}
+(*
+Ce fichier définit les fonctions nécessaires à la première question du projet, à savoir la création de mots de manière complètement aléatoire. N'utilisant aucun fichier, aucune table de probabilité, ce programme s'en trouve réduit au strict minimum.
+Comme dans les autres fichiers de projet, la fonction p1_main est appelée par le programme mère afin de générer un certain nombre de mots. Les variables n et s sont respectivement utilisées pour définir le nombre de mots et la longueur de ceux-ci.
+
+
+- 'alphabet' contient la liste de tout les caractères qu'il est possible de retrouver dans un mot. Le caractère spécial 'ø' est utilisé pour définir la fin d'un mot, il ne sera jamais affiché en sortie
+- 'creemotaleatoire' est une fonction qui crée un mot de contenu et longueur aléatoire, à partir de la taille minimale. Pour faire simple, la fonction ajoute des caractères un par un au mot jusqu'à trouver 'ø', indiquant la fin du mot
+- 'p1_main' est la procédure principale, appelée par le programme mère, et affiche n mots générés
+
+Author: Arthur Blaise (blaisearth@eisti.fr) - ©2018-2019
+*)
 
 
 // public  - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -21,9 +32,8 @@ implementation
 
 function creemotaleatoire (taille : integer):widestring ;
     var res : widestring;
-        c : unicodeString;
+        c : widestring;
     begin
-        // writeln('launched with ',taille);
         res :='';
         c := alphabet[random(length(alphabet))];
         while length(res)<taille do
@@ -34,7 +44,7 @@ function creemotaleatoire (taille : integer):widestring ;
                 c := alphabet[random(length(alphabet))+1];
                 end;
             while c='ø' do
-                c := alphabet[random(length(alphabet))];
+                c := alphabet[random(length(alphabet))+1];
             end;
         creemotaleatoire := res;
     end;
